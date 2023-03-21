420759eb5e66b4590cf30e7c5e8889004c405e70
Olivier Van Houwenhove <126786815+oli4VH@users.noreply.github.com>
2023-03-21 08:18:24 +0100

    Update files.py

--- Labo03/files.py
+++ Labo03/files.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+import os
+
+# Krijg een lijst van alle bestanden in de huidige map
+files = os.listdir()
+
+while True:
+    # Vraag de gebruiker om een bestandsnaam
+    filename = input("Voer een bestandsnaam in: ")
+
+    # Stop het script als de gebruiker niets invoert
+    if filename == "":
+        break
+
+    # Controleer of het bestand bestaat
+    if filename in files:
+        # Als het bestand bestaat, open het en print de inhoud
+        with open(filename, 'r') as file:
+            contents = file.read()
+            print(contents)
+    else:
+        # Als het bestand niet bestaat, vraag de gebruiker om een tekst en schrijf deze naar het bestand
+        text = input("Bestand bestaat niet. Voer tekst in om op te slaan: ")
+        with open(filename, 'w') as file:
+            file.write(text)
+            print("Bestand is aangemaakt en de tekst is opgeslagen.")
