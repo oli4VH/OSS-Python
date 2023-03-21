e985acb6be4457e4a0e68b0a98f9ab25fd718a1a
Olivier Van Houwenhove <126786815+oli4VH@users.noreply.github.com>
2023-03-21 08:15:38 +0100

    Update fgrep.py

--- Labo03/fgrep.py
+++ Labo03/fgrep.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+import sys
+
+# Haal het patroon op uit de eerste parameter
+pattern = sys.argv[1]
+
+# Loop door de bestanden die als parameters zijn meegegeven
+for file_path in sys.argv[2:]:
+    # Open het bestand
+    with open(file_path, 'r') as file:
+        # Loop door de regels in het bestand
+        for line in file:
+            # Zoek naar het patroon in de regel
+            if pattern in line:
+                # Als het patroon in de regel voorkomt, print dan de regel
+                print(line.strip())
