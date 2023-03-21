fe33a221cd27bdd1076857d6131ad55bc0a89b1d
Olivier Van Houwenhove <126786815+oli4VH@users.noreply.github.com>
2023-03-21 08:11:38 +0100

    Update lastuserdetails.py

--- Labo02/lastuserdetails.py
+++ Labo02/lastuserdetails.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+# Voer het commando uit om de laatste gebruiker uit /etc/passwd te halen
+last_user = subprocess.check_output("tail -n 1 /etc/passwd", shell=True)
+last_user = last_user.decode("utf-8").strip()
+
+# Splits de gebruikersinformatie in afzonderlijke velden
+last_user_fields = last_user.split(":")
+
+# Haal de gewenste informatie uit de velden
+username = last_user_fields[0]
+userid = last_user_fields[2]
+home_dir = last_user_fields[5]
+
+# Print de gebruikersinformatie
+print("Gebruikersnaam: ", username)
+print("User ID: ", userid)
+print("Home directory: ", home_dir)
