adc10d4fa4720978f9bc9df71c22f21faa07e097
Olivier Van Houwenhove <126786815+oli4VH@users.noreply.github.com>
2023-03-22 09:09:09 +0100

    Update print_file_greeting.py

--- Labo02/print_file_greeting.py
+++ Labo02/print_file_greeting.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python
+# open het bestand in read mode
+file = open('greeting.txt', 'r')
 
+# lees de inhoud van het bestand en sla deze op als een string
+content = file.read()
+
+# print de inhoud van het bestand
+print(content)
+
+# sluit het bestand
+file.close()
