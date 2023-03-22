318c5c0b501ffabd8ce00cfbe572a520e564c41b
Olivier Van Houwenhove <126786815+oli4VH@users.noreply.github.com>
2023-03-22 08:47:10 +0100

    Update echo.py

--- Labo03/echo.py
+++ Labo03/echo.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+import sys
+
+# alle command line argumenten, behalve de naam van het script
+for arg in sys.argv[1:]:
+    # Print het argument met een spatie als scheidingsteken
+    print(arg, end=' ')
+# Print een lege lijn
+print()
