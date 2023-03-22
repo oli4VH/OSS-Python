76a18e8f6f1b567c7cac7679fa6e78def94f44a1
Olivier Van Houwenhove <126786815+oli4VH@users.noreply.github.com>
2023-03-22 08:40:22 +0100

    Update cat.py

--- Labo03/cat.py
+++ Labo03/cat.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+# alle command line argumenten, behalve de naam van het script
+for arg in sys.argv[1:]:
+    # Open het bestand in read-only mode
+    with open(arg, 'r') as f:
+        # Print de inhoud van het bestand
+        print(f.read())
+    # Sluit het bestand
+    f.close()
