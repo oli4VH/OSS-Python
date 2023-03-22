31561a567120e19c0f39269015ca3cbbb142b5ae
Olivier Van Houwenhove <126786815+oli4VH@users.noreply.github.com>
2023-03-22 09:11:37 +0100

    Update write_greeting.py

--- Labo02/write_greeting.py
+++ Labo02/write_greeting.py
@@ -1,1 +1,9 @@
+#!/usr/bin/python3
+# open het bestand in write mode
+file = open('greeting.txt', 'w')
 
+# schrijf een groet naar het bestand
+file.write("Gegroet!\n")
+
+# sluit het bestand
+file.close()
