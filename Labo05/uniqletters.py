6295be5c726e30878534b5da5d9812237f91c235
Olivier Van Houwenhove <126786815+oli4VH@users.noreply.github.com>
2023-03-24 09:08:16 +0100

    Update uniqletters.py

--- Labo05/uniqletters.py
+++ Labo05/uniqletters.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uniqletters(string, count=1, ignore_case=False):
+    if ignore_case:
+        string = string.lower()
+    resultaat = ''
+    for letter in string:
+        if resultaat.count(letter) < count:
+            resultaat += letter
+    return resultaat
