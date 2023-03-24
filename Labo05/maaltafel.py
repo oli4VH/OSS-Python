c7ccfe1d239aa756553ef58b6cc4dec1a0a86f68
Olivier Van Houwenhove <126786815+oli4VH@users.noreply.github.com>
2023-03-24 09:06:31 +0100

    Update maaltafel.py

--- Labo05/maaltafel.py
+++ Labo05/maaltafel.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def maaltafel(getal, maximum=10):
+    maalresultaten = []
+    for i in range(1, maximum+1):
+        maalresultaten.append(getal * i)
+    return maalresultaten
